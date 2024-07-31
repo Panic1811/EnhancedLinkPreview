22e116fb0e6e9fd84b5e2c047178b5f40a79e72b
Panic1811 <56132791+Panic1811@users.noreply.github.com>
2024-07-31 16:03:39 +0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
-document.getElementById('options').addEventListener('click', function() {
-  chrome.runtime.openOptionsPage();
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.storage.sync.get({enabled: true}, function(items) {
+    document.getElementById('enabled').checked = items.enabled;
+  });
+
+  document.getElementById('enabled').addEventListener('change', function() {
+    chrome.storage.sync.set({enabled: this.checked});
+  });
+
+  document.getElementById('options').addEventListener('click', function() {
+    chrome.runtime.openOptionsPage();
+  });
 });
